9bed1b47cc976dae17d496a2e128536333d10552
Alok Kumar Rawat <149565273+AlokKumarRawat@users.noreply.github.com>
2024-06-05 15:01:12 +0530

    Create Question3.c

---
+++ Question3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    printf("The size of interger is %d\n", sizeof(int));
+    printf("The size of float is %d\n", sizeof(float));
+    printf("The size of double is %d\n", sizeof(double));
+    printf("The size of character is %d", sizeof(char));
+    return 0;
+}
